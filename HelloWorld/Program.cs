6d2f35e326d2bb2ed8dc4ee21691e448b3bda6f0
Juan Pablo Salazar <juansalazar1121361@correo.itm.edu.co>
2026-04-16 22:52:40 -0500

    se crea ejercicio1 =cálculo de potencia positiva y ejercicio2 = lógica doble o triple

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Helloword
+{
+    class Program{
+       
+     static void Main()
+    {
+        int opcion;
+
+        do
+        {
+            Console.WriteLine("\n===== MENÚ DE EJERCICIOS =====");
+            Console.WriteLine("1. Positive Power");
+            Console.WriteLine("2. Double or Triple");
+            Console.WriteLine("3. Root or Square");
+            Console.WriteLine("4. Circle Perimeter");
+            Console.WriteLine("5. Midweek Day");
+            Console.WriteLine("6. Tax Calculator");
+            Console.WriteLine("7. Remainder Finder");
+            Console.WriteLine("8. Sum of Evens");
+            Console.WriteLine("9. Fraction Difference");
+            Console.WriteLine("10. String Length");
+            Console.WriteLine("11. Average of Four");
+            Console.WriteLine("12. Smallest of Five");
+            Console.WriteLine("13. Vowel Counter");
+            Console.WriteLine("14. Factorial Finder");
+            Console.WriteLine("15. InRange Validator");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+
+            opcion = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+ switch (opcion)
+            {
+                case 1:
+                    Console.Write("Ingrese un número: ");
+                    int num1 = int.Parse(Console.ReadLine());
+                    if (num1 > 0)
+                        Console.WriteLine(num1 * num1);
+                    else if (num1 == 0)
+                        Console.WriteLine(0);
+                    else
+                        Console.WriteLine("Número negativo.");
+                    break;
+
+                case 2:
+                    Console.Write("Ingrese el primer número: ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese el segundo número: ");
+                    int b = int.Parse(Console.ReadLine());
+
+                    if (a >= b)
+                        Console.WriteLine(a * 2);
+                    else
+                        Console.WriteLine(b * 3);
+                    break;
+
+}
+    }
+}
+
+}
